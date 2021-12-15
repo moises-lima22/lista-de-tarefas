a9246d9c6a415b9c3358b2f77ab815fef1293a8c
Moises Silva <moises.silva2015@outlook.com>
2021-12-15 13:48:47 -0300

    Adicionando tarefas

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,35 +1,46 @@
-import React, { useState } from 'react';
-import './App.css';
-import './index.css';
-import './components/Tasks'
-import Tasks from './components/Tasks';
-import AddTask from './components/AddTask';
+import React, { useState } from "react";
+import {v4 as uuidv4} from 'uuid'
+
+import "./App.css";
+import "./index.css";
+import "./components/Tasks";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 const App = () => {
   // let message = "Mariass Alice"
   const [tasks, setTasks] = useState([
     {
-      id: '1',
-      title: 'Estudar React',
+      id: "1",
+      title: "Estudar React",
       completed: false,
     },
     {
-      id: '2',
-      title: 'Estudar Git Flow',
+      id: "2",
+      title: "Estudar Git Flow",
       completed: true,
     },
     {
-      id: '2',
-      title: 'Estudar Git Flow',
+      id: "2",
+      title: "Estudar Git Flow",
       completed: true,
     },
   ]);
 
+  const handleTaskAddition = (taskTitle) => {
+    const newTasks = [
+      ...tasks,
+      { title: taskTitle, id: uuidv4(), completed: true },
+    ];
+
+    setTasks(newTasks);
+  };
+
   return (
     <>
-      <div className='container'>
-        <AddTask/>
-        <Tasks tasks={tasks}/>
+      <div className="container">
+        <AddTask handleTaskAddition={handleTaskAddition} />
+        <Tasks tasks={tasks} />
       </div>
     </>
   );
